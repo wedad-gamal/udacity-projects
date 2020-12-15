e1ba9df735bf12dde0571ce5f79f64cf8678cfcd
wedad <wedad.gamal@gmail.com>
2020-12-15 20:08:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Weather-Journal App Project
+
+## Overview
+This project is asynchronous web app that uses Web API and user data to dynamically update the UI. 
+
+## Instructions
+server.js uses port 3000
+server.js is pointing to website folder which has index.html
+using this API url by using API key => api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+API documentation:  https://openweathermap.org/current
---
+++ website/app.js
@@ -0,0 +1,91 @@
+/* Global Variables */
+
+// personal API key for OpenWeatherMap API
+const key ='&appid=7162708c931f31f126dff1d50fbfcd96&units=imperial';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+let zipCode='giza,12511';
+let userResponse = '';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = (d.getMonth()+1)+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+// Async POST
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      console.log(newData);
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+};
+
+
+
+const generateProcess = async ()=>{
+ 
+    zipCode = document.getElementById('zip').value;
+    userResponse = document.getElementById('feelings').value;
+
+  	const url = baseURL+zipCode+key;
+  	console.log(url);
+    getWeatherData(url)
+  	 .then(function(data){
+      console.log(data);
+      let temperature = '';
+      if(data.cod == "200"){
+        temperature = data.main.temp;
+      }else{
+        temperature = data.message;
+      }
+      postData('/addWeatherData',{temperature:temperature , newDate: newDate, userResponse: userResponse})
+
+      updateUI()
+    })
+}
+
+document.getElementById('generate').addEventListener('click',generateProcess);
+
+const getWeatherData = async (url)=>{
+	
+  	const res = await fetch(url)
+  	try{
+   	 const data = await res.json();
+     console.log(data);
+     return data;
+	 }catch(error){
+    console.log("error", error);
+  }
+}
+
+
+const updateUI = async () =>{
+  const request =await fetch('/getWeatherData')
+  try{
+    const allData = await request.json();
+    console.log(allData);
+    document.getElementById('date').innerHTML = `Temperature: ${allData.temperature}`;
+    document.getElementById('temp').innerHTML = `Date: ${allData.newDate}`;
+    document.getElementById('content').innerHTML = `User Input: ${allData.userResponse}`;
+
+  }catch(error){
+    console.log("error",error);
+
+  }
+}
+
+
+
